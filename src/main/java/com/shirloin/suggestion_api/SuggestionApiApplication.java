99826a61be6f5e6177be10ef2e57e7477d0701b2
Shirloin <riccardo@binus.ac.id>
2024-11-22 20:38:19 +0700

    first commit

---
+++ src/main/java/com/shirloin/suggestion_api/SuggestionApiApplication.java
@@ -0,0 +1,13 @@
+package com.shirloin.suggestion_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuggestionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuggestionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shirloin/suggestion_api/SuggestionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shirloin.suggestion_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuggestionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
