c8faa8587acc735fc19ce1640f6883c16e020b37
Shirloin <riccardo@binus.ac.id>
2024-11-24 16:30:31 +0700

    fix: unit testing redis host

--- src/test/java/com/shirloin/suggestion_api/SuggestionControllerTest.java
+++ src/test/java/com/shirloin/suggestion_api/SuggestionControllerTest.java
@@ -3,6 +3,7 @@ package com.shirloin.suggestion_api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.shirloin.suggestion_api.model.CitySuggestion;
@@ -17,6 +18,7 @@ import org.junit.jupiter.api.Test;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class SuggestionControllerTest {
 
     @Autowired
